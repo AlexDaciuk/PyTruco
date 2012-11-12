29e25727d37c3372027ee486a329f03820ff1438
AlexDaciuk <adaciuk@gmail.com>
2012-11-12 20:11:10 -0300

    Croupier goes object 0.4

--- croupier/croupier_object.py
+++ croupier/croupier_object.py
@@ -1,8 +1,10 @@
 #we import random from choice
 from random import choice
 players = []
+
 class deck:
     def __init__(self):
+        global modifiable_deck
         deck = []
         self.deck = deck
         types = ["oro","basto","espada","copas"]
@@ -10,39 +12,39 @@ class deck:
             for i in list(range(1,13,1)):
                 if i == 8 or i == 9:
                     pass
-                else:
-                    deck.append(str(i)+"_de_"+card_type)
+            else:
+                deck.append(str(i)+"_de_"+card_type)
         modifiable_deck = deck
-                
-
-    def croupier(players = [], *args):
+    
+    def croupier(self, players = [], *args):
         for player in players:
             random_card = choice(modifiable_deck)
             player.card_list.append(random_card)
             modifiable_deck.remove(random_card)
             player.card_quantity += 1
-
-    
+            
     def print_deck(self):
-        print(self.deck)
-
-
+            print(self.deck)
+                    
+                    
 class player:
     def __init__(self):
         card_quantity = 0
+        self.card_quantity = card_quantity
         card_list = []
         self.card_list = card_list
-
+        players.append(self)
+        
     def show_cards(self):
         print(self.card_list)
-
+        
 deck1 = deck()
 
 player_1 = player()
-players.append("player_1")
 
 player_2 = player()
-players.append("player_2")
+
+deck1.croupier(players)
 
 player_1.show_cards()
 player_2.show_cards()
