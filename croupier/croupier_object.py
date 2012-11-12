3dc64a78d068c5d774633cc1b826e569dedc4b77
AlexDaciuk <adaciuk@gmail.com>
2012-11-12 18:50:10 -0300

    Croupier going object 0.2

--- croupier/croupier_object.py
+++ croupier/croupier_object.py
@@ -9,13 +9,27 @@ class deck:
         for card_type in types:
             for i in list(range(1,13,1)):
                 deck.append(str(i)+"_de_"+card_type)
-        
+        modifiable_deck = deck
                 
+
+    def croupier(players = [], *args):
+        for player in players:
+            random_card = choice(modifiable_deck)
+            player.card_list.append(random_card)
+            modifiable_deck.remove(random_card)
+            player.card_quantity += 1
+
+    
     def print_deck(self):
         print(self.deck)
 
 
-
+class player:
+    def __init__(self):
+        card_quantity = 0
+        card_list = []
+        
+    
 
 deck1 = deck()
 
