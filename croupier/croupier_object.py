12c246a61545a1d3ae21dfad7dca04eb6bba1919
AlexDaciuk <adaciuk@gmail.com>
2012-11-12 20:30:16 -0300

    Croupier goes object 0.5 (Works)

--- croupier/croupier_object.py
+++ croupier/croupier_object.py
@@ -4,7 +4,6 @@ players = []
 
 class deck:
     def __init__(self):
-        global modifiable_deck
         deck = []
         self.deck = deck
         types = ["oro","basto","espada","copas"]
@@ -12,16 +11,18 @@ class deck:
             for i in list(range(1,13,1)):
                 if i == 8 or i == 9:
                     pass
-            else:
-                deck.append(str(i)+"_de_"+card_type)
+                else:
+                    deck.append(str(i)+"_de_"+card_type)
         modifiable_deck = deck
-    
+        self.modifiable_deck = modifiable_deck
+        
     def croupier(self, players = [], *args):
         for player in players:
-            random_card = choice(modifiable_deck)
-            player.card_list.append(random_card)
-            modifiable_deck.remove(random_card)
-            player.card_quantity += 1
+            for i in range(3):
+                random_card = choice(self.modifiable_deck)
+                player.card_list.append(random_card)
+                self.modifiable_deck.remove(random_card)
+                player.card_quantity += 1
             
     def print_deck(self):
             print(self.deck)
