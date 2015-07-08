888f7ddd66e90c0b84c4196313ea196f51d9363e
Alexis Daciuk <adaciuk@gmail.com>
2015-07-08 06:39:53 -0300

    Implement the class 'Deck' (not complete)

---
+++ class/deck.py
@@ -0,0 +1,17 @@
+# Class that defines a deck of cards, basically is a list with all the cards
+import Card
+
+class Deck():
+
+    def __init__():
+        
+        for number in range(1,12,1):
+            
+            for figure in ("ORO", "BASTO", "ESPADA", "COPA"):
+
+                if ( number == 8 or number == 9):
+                    pass
+                else:
+                    cardList.append( card(number,figure) )
+                    
+                
